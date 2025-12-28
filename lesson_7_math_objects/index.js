007e72d6ce0d414f2b00179b421456d4c41dbda2
Acer <natsuaryal13@gmail.com>
2025-12-28 19:43:34 +0545

    feat: math objects

---
+++ lesson_7_math_objects/index.js
@@ -0,0 +1,30 @@
+// Math.PI gives exact value of pie and so on
+
+// console.log(Math.PI);
+// console.log(Math.E);
+
+let x = 3.59;
+let y = -10;
+let u = 81;
+let z = 90;
+
+// z = Math.round(x); // rounds decmial number.
+// z = Math.floor(x); //floor for example in 4.99 the floor will be 4
+// z = Math.ceil(x); // for example 4.12 the ceil will be 5
+// z = Math.trunc(x); // eliminates any decmial portion
+// z = Math.pow(y,u); // use to raise power
+// z = Math.sqrt(u); // use to find square root
+// z = Math.log(y); //use to find the natural log 
+// z= Math.abs(y); // use to find absolute value
+// z = Math.sign(y); //it is sue to find the sign of number
+
+// there is Math.sin(45), Math.cos() , Math.tan()
+
+let max = Math.max(x,y,z,u); // use to display max  value among variables
+let min = Math.min(x,y,z,u); // use to display min value between variables
+
+
+
+console.log(max);
+console.log(min);
+
