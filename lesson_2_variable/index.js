302c7abe655ed456349cbf1ac03e68971f2718f7
Acer <natsuaryal13@gmail.com>
2025-12-28 18:51:57 +0545

    docs: added comments

--- lesson_2_variable/index.js
+++ lesson_2_variable/index.js
@@ -1,22 +1,24 @@
-// let x = 100
-// let price = 10.99
-// let userName = "ramesh"
-// let online = true
+// variable decleration and assignment
+let x = 100
+let price = 10.99
+let userName = "ramesh"
+let online = true
 
-// // you have to use `` to be able to use ${variable}
-// console.log(`you are ${x} years old`)
+// you have to use `` to be able to use ${variable}
+console.log(`you are ${x} years old`)
 
-// // gives the datatype of variable (number, string , boolean)
-// console.log(typeof price)
-// console.log(typeof userName)
-// console.log(typeof online)
+// gives the datatype of variable (number, string , boolean)
+console.log(typeof price)
+console.log(typeof userName)
+console.log(typeof online)
 
-// console.log(`Bro is online: ${online}`)
+console.log(`Bro is online: ${online}`)
 
 let fullName = "Samip Aryal"
 let age = 18
 let isstudent = true
 
+//printing value
 document.getElementById("p1").textContent = `your full name is ${fullName}`
 document.getElementById("p2").textContent = `your age is ${age}`
 document.getElementById("p3").textContent = `you are a student: ${isstudent}`
