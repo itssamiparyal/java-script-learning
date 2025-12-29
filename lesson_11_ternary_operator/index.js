78e9dfaea453d20c92c4975806306121c893a046
Acer <natsuaryal13@gmail.com>
2025-12-29 20:43:50 +0545

    feat: add  ternary operator example

---
+++ lesson_11_ternary_operator/index.js
@@ -0,0 +1,14 @@
+// let age = 25;
+
+// let message = age >= 18 ? "you are an adult" : "you are a minor"
+// console.log(message);
+
+// let time = 16;
+
+// let greeting = time < 12? "Good Morning!" : "Good Afternoon";
+// console.log(greeting);
+
+let purchaseAmount = 99;
+let discount = purchaseAmount >= 100 ? 10:0;
+
+console.log(`your total amount is $${purchaseAmount - purchaseAmount *(discount/100)}`);
