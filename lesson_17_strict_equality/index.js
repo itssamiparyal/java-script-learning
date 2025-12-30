8f194630cbdc61c83cc51bf313e0e0e46444840e
Acer <natsuaryal13@gmail.com>
2025-12-30 19:59:08 +0545

    feat: add example of strict equality

---
+++ lesson_17_strict_equality/index.js
@@ -0,0 +1,20 @@
+// strict equality
+// = assignment operator
+// == comparison operator (compare if value are equal)
+// === strict equality operator (compare if datatype & value are equal)
+// != inequality operator
+// !== Strict inequality operator
+
+const PI = 3.14;
+
+if (PI === "3.14") {
+  console.log("That is PI");
+} else {
+  console.log("that is not PI");
+}
+
+if (PI !== "3.14") {
+  console.log("That is PI");
+} else {
+  console.log("that is not PI");
+}
