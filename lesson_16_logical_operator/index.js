ca72539abb86c414c3d641dc67e291708de3787c
Acer <natsuaryal13@gmail.com>
2025-12-30 19:46:12 +0545

    feat: add example of logical operators

---
+++ lesson_16_logical_operator/index.js
@@ -0,0 +1,9 @@
+// logical operators && is and || is or ! is not; not true is false and vise versa
+
+const temprature = 20;
+
+if (temprature > 0 && temprature < 30) {
+  console.log("The temprature is good!");
+} else {
+  console.log("The Temperature is bad!");
+}
