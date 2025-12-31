61b288b08aa14118354dd8dadfdeb1ea59fcc4b8
Acer <natsuaryal13@gmail.com>
2025-12-31 20:43:33 +0545

    feat: add example of array

---
+++ folder_21_array/index.js
@@ -0,0 +1,25 @@
+//array
+
+let fruits = ["apple", "orange", "bananna"];
+fruits.sort().reverse();
+
+// fruits[0] = "coconut";
+// fruits.push("coconut"); // adds coconut to the end
+// fruits.pop(); // removes last element
+// fruits.unshift("mango"); // adds mango to the start
+// fruits.shift(); // removes element from beging
+
+// console.log(fruits[0]);
+
+// let numOfFruits = fruits.length;
+// let index = fruits.indexOf("apple");
+// console.log(index);
+// console.log(numOfFruits);
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+for (let fruit of fruits) {
+  console.log(fruits);
+}
